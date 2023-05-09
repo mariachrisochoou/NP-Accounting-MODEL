08f97f364f002584de45157f301f3c91c7efee58
Marina Gkolfinopoulou <115397442+MarinaGolf12@users.noreply.github.com>
2023-05-10 00:06:24 +0300

    Update searchsortjavascripthelpers.js

--- SearchDocsToCompare/searchsortjavascripthelpers.js
+++ SearchDocsToCompare/searchsortjavascripthelpers.js
@@ -1,21 +1,3 @@
-
-    /*function search() {
-        var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById("search-holder");
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("myUL");
-        li = ul.getElementsByTagName("li");
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("a")[0];
-            txtValue = a.textContent || a.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none"; 
-            }
-        }
-    }*/
-
 function sortList() {
   var ul = document.getElementById("myUL");
   var li = ul.querySelectorAll("li");
